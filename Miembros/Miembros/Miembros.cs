606484c1cb336c6ff93145ca6e4df063ffe18b59
Vicente <macantivbl@gmail.com>
2022-09-12 14:04:37 -0500

    Destructores y Miembros

---
+++ Miembros/Miembros/Miembros.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miembros
+{
+    internal class Miembros
+    {
+        //Miembros - Campos Privados
+        private string nombrePersona;
+        private string tituloTrabajo;
+        private int salario;
+
+        // Miembros Campo Publico
+        public int edad;
+
+        //Miembros - Propiedades 
+        public string TituloTrabajo
+        {
+            get
+            {
+                return tituloTrabajo;
+            }
+            set
+            {
+                tituloTrabajo = value;
+            }
+        }
+
+        //Miembros - Metodo privado
+        private void CompartirInfoPrivada()
+        {
+            Console.WriteLine("Mi salario es: {0}", salario);
+        }
+
+        //miembros - metodos publicos
+        public void Amigo(bool esAmigo)
+        {
+            if (esAmigo)
+            {
+                CompartirInfoPrivada();
+            }
+            Console.WriteLine("no tiene permisos para ver esto");
+        }
+
+        //Miembros - Constructor
+
+        public Miembros()
+        {
+            edad = 30;
+            nombrePersona = "Ana";
+            salario = 50000;
+            tituloTrabajo = "Desarrollador";
+            Console.WriteLine("objeto Creado");
+        }
+
+        //Destructor
+        ~Miembros()
+        {
+            //Limpia declaraciones
+            Console.WriteLine("Destruccion del objeto miembro");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Miembros/Miembros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miembros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Miembros nuevoMiembros = new Miembros();
+            nuevoMiembros.Amigo(true);
+
+            Console.ReadLine();
+        }
+    }
+}
