b3cd3573b356ed6827331afb42e6eae30069dcd5
Vicente <macantivbl@gmail.com>
2022-09-12 19:12:20 -0500

    arreglo unidimencional

---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] calificaciones = new double[5];
+
+            calificaciones[0] = 10;
+            calificaciones[1] = 7.5;
+            calificaciones[2] = 8.2;
+            calificaciones[3] = 6.8;
+            calificaciones[4] = 5.5;
+
+            // otra manera de crear arrays
+            int[] edades = { 17, 15, 16, 15, 17 };
+
+            // otra manera de crear arrays
+            int[] faltasAlColegio = new int[] { 3, 5, 7, 0, 4 };
+
+            for (int y = 0; y < calificaciones.Length; y++)
+            {
+                Console.WriteLine("Ingrese la calificacion: ");
+                string ingreso = Console.ReadLine();
+                calificaciones[y] = Double.Parse(ingreso);
+            }
+
+            foreach (double i in calificaciones)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ arreglosMultidimencionales/arreglosMultidimencionales/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglosMultidimencionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array 2d
+            string[,] matriz2d;
+
+            //Array 3d
+            string[,,] matriz3d;
+
+            string[,] array2d = new string[,]
+            {
+                {"A","B","C" },
+                {"D","E","F"},
+                {"G","H","I"}
+            };
+
+            Console.WriteLine("El valor central es {0}", array2d[2,0]);
+            Console.WriteLine("La dimension del arreglo es {0}", array2d.Rank);
+            Console.Read();
+        }
+    }
+}
